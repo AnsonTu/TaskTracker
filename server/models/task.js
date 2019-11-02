9ea3176e2a167d4004f864a0da31e845911bd5c8
meganhng <meganhong17@hotmail.com>
2019-11-02 16:41:03 -0400

    Task Schema: Adding the task schema and hopefully API calls

---
+++ server/models/task.js
@@ -0,0 +1,14 @@
+import { stringify } from "querystring";
+
+const mongoose = required('mongoose');
+const Schema = mongoose.Schema;
+
+const taskSchema = new Schema({
+    task:{type: String, required:true},
+    description:{type:String},
+    completion: {type:Boolean, required:true},
+    dueDate:{type:Date, required: true},
+});
+
+const TaskClass = mongoose.model('task', taskSchema);
+module.exports = TaskClass;
