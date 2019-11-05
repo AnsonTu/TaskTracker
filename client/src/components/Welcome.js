9d3b8badb4c6e5c2a49497592876523fb49130ba
Anson Tu <anson9137@gmail.com>
2019-11-04 21:03:40 -0500

    WIP: Added helmet and styled pages

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -1,5 +1,36 @@
 import React from "react";
+import { Helmet } from "react-helmet";
+import styled from "styled-components";
 
-export default () => {
-  return <h3>Welcome! Sign up or sign in!</h3>;
-};
+function Welcome() {
+  return (
+    <Container>
+      <Helmet>
+        <title>Welcome | Task Tracker</title>
+      </Helmet>
+      <Content>
+        <h3>Welcome! Sign up or sign in!</h3>
+      </Content>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: black;
+  main {
+    flex-direction: column;
+    display: flex;
+  }
+`;
+
+const Content = styled.div`
+  position: relative;
+  background-color: lightgray;
+  width: 50%;
+  margin: auto;
+`;
+
+export default Welcome;
--- client/src/components/auth/Signup.js
+++ client/src/components/auth/Signup.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
+import { Helmet } from "react-helmet";
 import { reduxForm, Field } from "redux-form";
 import { compose } from "redux";
 import { connect } from "react-redux";
 import * as actions from "../../actions";
+import styled from "styled-components";
 
 class Signup extends Component {
   // On submitting the form, pass in the email and password,
@@ -17,31 +19,87 @@ class Signup extends Component {
   render() {
     const { handleSubmit } = this.props;
     return (
-      <form onSubmit={handleSubmit(this.onSubmit)}>
-        <fieldset>
-          <label>Email</label>
-          <Field
-            name="email"
-            type="text"
-            component="input"
-            autoComplete="none"
-          />
-        </fieldset>
-        <fieldset>
-          <label>Password</label>
-          <Field
-            name="password"
-            type="password"
-            component="input"
-            autoComplete="new-password"
-          />
-        </fieldset>
-        <div>{this.props.errorMessage}</div>
-        <button>Sign Up!</button>
-      </form>
+      <Container>
+        <Helmet>
+          <title>Sign Up | Task Tracker</title>
+        </Helmet>
+        <Content>
+          <form onSubmit={handleSubmit(this.onSubmit)}>
+            <h2>Sign up with your email address</h2>
+            <InputField>
+              <Field
+                name="email"
+                type="text"
+                component="input"
+                autoComplete="none"
+                placeholder="Email"
+              />
+            </InputField>
+            <InputField>
+              <Field
+                name="password"
+                type="password"
+                component="input"
+                autoComplete="new-password"
+                placeholder="Password"
+              />
+            </InputField>
+            <InputField>
+              <SubmitButton>SIGN UP</SubmitButton>
+            </InputField>
+            <OutputField>{this.props.errorMessage}</OutputField>
+          </form>
+        </Content>
+      </Container>
     );
   }
 }
+
+const Container = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: black;
+  main {
+    flex-direction: column;
+    display: flex;
+  }
+`;
+
+const Content = styled.div`
+  position: relative;
+  background-color: lightgray;
+  width: 50%;
+  height: 50%;
+  margin: 0 auto;
+  padding-top: 10%
+  display: flex;
+  justify-content: center;
+`;
+
+const InputField = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  text-align: center;
+`;
+
+const OutputField = styled(InputField)`
+  margin-bottom: 10px;
+`;
+
+const SubmitButton = styled.button`
+  background-color: lightblue;
+  margin: 20px;
+  padding: 20px;
+  padding: 20px;
+  :hover {
+    background-color: skyblue;
+  }
+  :active {
+    background-color: teal;
+  }
+`;
 
 // Set the error message if the user tries to sign up
 // with an email that was already used
