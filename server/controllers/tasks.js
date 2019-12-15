f24644da41d28f41e48f5442e7a23fda4741abea
AnsonTu <anson9137@gmail.com>
2019-12-14 20:21:28 -0500

    Ran prettier across files and added UserId to task model

--- server/controllers/tasks.js
+++ server/controllers/tasks.js
@@ -30,21 +30,23 @@ exports.deleteTask = function(req, res, next) {
 exports.getTasks = function(req, res, next) {
   Tasks.find()
     .then(tasks => res.json(tasks))
-    .catch(err => res.status(400).json("Error" + err));
+    .catch(err => res.status(400).json("Error:" + err));
 };
 
 //this will probably change, placeholder for now when I get a better feel for the UI i guess?
 exports.updateTasks = function(req, res, next) {
-  Tasks.findById(req.params.id).then(task => {
-    task.task = req.body.task;
-    task.description = req.body.description;
-    task.dueDate = req.body.dueDate;
-    task.completion = req.body.completion;
-    task.startDate = req.body.startDate;
+  Tasks.findById(req.params.id)
+    .then(task => {
+      task.task = req.body.task;
+      task.description = req.body.description;
+      task.dueDate = req.body.dueDate;
+      task.completion = req.body.completion;
+      task.startDate = req.body.startDate;
 
-    task.save()
-    .then(() => res.json("Task was updated!"))
-    .catch(err => res.status(400).json("Error: "+err))
-  })
-  .catch(err => res.status(400).json("Error: "+ err));
+      task
+        .save()
+        .then(() => res.json("Task was updated!"))
+        .catch(err => res.status(400).json("Error: " + err));
+    })
+    .catch(err => res.status(400).json("Error: " + err));
 };
