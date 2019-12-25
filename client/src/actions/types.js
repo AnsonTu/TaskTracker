3ca9cc4cd0b373a10e181d35914564708d3b97fe
AnsonTu <anson9137@gmail.com>
2019-12-25 17:20:32 -0500

    Refactored action files/folders and added actions for fetching tasks

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,2 +1,7 @@
+// Action types for authentication
 export const AUTH_USER = "auth_user";
 export const AUTH_ERROR = "auth_error";
+
+// Action types for tasks
+export const FETCH_TASKS = "fetch_tasks";
+export const FETCH_FAILED = "fetch_failed";
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import { reducer as formReducer } from "redux-form";
 import auth from "./auth";
+import task from "./task";
 
 export default combineReducers({
   auth,
+  task,
   form: formReducer
 });
