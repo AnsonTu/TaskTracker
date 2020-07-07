aba0879ce7f4d7c3488e09da2bd06427ce66cb67
AnsonTu <anson9137@gmail.com>
2020-07-07 01:52:47 -0400

    Added ButtonLink component and updated header bar

--- client/src/components/HeaderBar/index.js
+++ client/src/components/HeaderBar/index.js
@@ -1,38 +1,24 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import styled from "styled-components";
+import HomeIcon from "@material-ui/icons/Home";
+import ButtonLink from "../ButtonLink";
 
 const HeaderBar = ({ authenticated }) => {
   return (
     authenticated && (
       <AppBar position="static">
-        <Toolbar style={{ backgroundColor: "#691e82" }}>
-          <HeaderLink to="/signout">Sign Out</HeaderLink>
-          <HeaderLink to="/home">Home Page</HeaderLink>
+        <Toolbar variant="dense" style={{ backgroundColor: "#691e82" }}>
+          <ButtonLink to="/home">
+            <HomeIcon />
+          </ButtonLink>
+          <ButtonLink to="/signout">Sign Out</ButtonLink>
         </Toolbar>
       </AppBar>
     )
   );
 };
-
-const HeaderLink = styled(Link)`
-  padding: 8px 20px;
-  background-color: #691e82;
-  border-right: 2px solid #000000;
-  text-decoration: none;
-  font-size: 24px;
-  font-weight: bold;
-  &:hover {
-    background-color: lightgray;
-  }
-  &:link,
-  &:visited {
-    color: #ffffff;
-  }
-`;
 
 function mapStateToProps(state) {
   return { authenticated: state.auth.authenticated };
