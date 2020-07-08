c49fed72338bb31978aed86e96bf6e862aa779ca
AnsonTu <anson9137@gmail.com>
2020-07-07 21:28:47 -0400

    Set types for sign up and sign in  fields

--- client/src/pages/authentication/components/ReduxTextField/index.js
+++ client/src/pages/authentication/components/ReduxTextField/index.js
@@ -2,11 +2,16 @@ import React from "react";
 import TextField from "@material-ui/core/TextField";
 
 const ReduxTextField = (props) => {
-  const { label, error } = props;
+  const {
+    input: { name },
+    label,
+    error
+  } = props;
 
   return (
     <TextField
       style={{ width: "60%", paddingBottom: "1.5rem" }}
+      type={name}
       placeholder={label}
       error={!!error}
     />
