350bdf373550ef550fb6c13c558e379db31bfb11
AnsonTu <anson9137@gmail.com>
2020-07-10 20:45:21 -0400

    Fixed bug with signin/signup fields, added label

--- client/src/pages/authentication/components/ReduxTextField/index.js
+++ client/src/pages/authentication/components/ReduxTextField/index.js
@@ -2,14 +2,15 @@ import React from "react";
 import TextField from "@material-ui/core/TextField";
 
 const ReduxTextField = (props) => {
-  const { type, label, error } = props;
+  const { type, label, error, input } = props;
 
   return (
     <TextField
+      label={label}
       style={{ width: "60%", paddingBottom: "1.5rem" }}
       type={type ? type : "text"}
-      placeholder={label}
       error={!!error}
+      {...input}
     />
   );
 };
