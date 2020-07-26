ab137b1cb63e9a99f09af8223b545affe01b26ce
AnsonTu <anson9137@gmail.com>
2020-07-26 04:46:54 -0400

    Added new types for updating and deleting tasks

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -8,3 +8,7 @@ export const FETCH_TASKS = "fetch_tasks";
 export const FETCH_FAILED = "fetch_failed";
 export const CREATE_TASK = "create_task";
 export const CREATE_TASK_FAILED = "create_task_failed";
+export const UPDATE_TASK = "update_task";
+export const UPDATE_TASK_FAILED = "update_task_failed";
+export const DELETE_TASK = "delete_task";
+export const DELETE_TASK_FAILED = "delete_task_failed";
